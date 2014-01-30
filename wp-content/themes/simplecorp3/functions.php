aa7578488a892a2841ab09b70ad709df2c811dfe
Ryan Kallis <ryan.kallis@cloudbi.co.za>
2014-01-30 15:48:01 +0200

    site themed

--- wp-content/themes/simplecorp3/functions.php
+++ wp-content/themes/simplecorp3/functions.php
@@ -1,31 +1,52 @@
 <?php
-require_once('library/siteframework.php');		// core functions
-require('theme-options.php');          			// theme options
+	require_once('library/siteframework.php');		// core functions
+	require('theme-options.php');          			// theme options
 
-add_action( 'add_meta_boxes', 'action_add_meta_boxes', 10, 2 );
-function action_add_meta_boxes() {
-	global $_wp_post_type_features;
-	if (isset($_wp_post_type_features['post']['editor']) && $_wp_post_type_features['post']['editor']) {
-		unset($_wp_post_type_features['post']['editor']);
-		add_meta_box(
-			'description_section',
-			__('Editor'),
-			'inner_custom_box',
-			'post', 'normal', 'default'
-		);
-	}
-	if (isset($_wp_post_type_features['page']['editor']) && $_wp_post_type_features['page']['editor']) {
-		unset($_wp_post_type_features['page']['editor']);
-		add_meta_box(
-			'description_sectionid',
-			__('Editor'),
-			'inner_custom_box',
-			'page', 'normal', 'default'
-		);
-	}
-}
-function inner_custom_box( $post ) {
-	the_editor($post->post_content);
-}
+	add_action( 'add_meta_boxes', 'action_add_meta_boxes', 10, 2 );
+	function action_add_meta_boxes() {
+		global $_wp_post_type_features;
+		if (isset($_wp_post_type_features['post']['editor']) && $_wp_post_type_features['post']['editor']) {
+			unset($_wp_post_type_features['post']['editor']);
+			add_meta_box(
+				'description_section',
+				__('Editor'),
+				'inner_custom_box',
+				'post', 'normal', 'default'
+			);
+		}
 
-    ?>
+		if (isset($_wp_post_type_features['page']['editor']) && $_wp_post_type_features['page']['editor']) {
+			unset($_wp_post_type_features['page']['editor']);
+			add_meta_box(
+				'description_sectionid',
+				__('Editor'),
+				'inner_custom_box',
+				'page', 'normal', 'default'
+			);
+		}
+	}
+	function inner_custom_box( $post ) {
+		the_editor($post->post_content);
+	}
+
+	remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+	add_filter('get_the_excerpt', 'custom_trim_excerpt');
+	function custom_trim_excerpt($text = '')
+	{
+		$raw_excerpt = $text;
+		if ( '' == $text ) {
+			$text = get_the_content('');
+	 
+			//$text = strip_shortcodes( $text );
+	 
+			$text = apply_filters('the_content', $text);
+			$text = str_replace(']]&gt;', ']]&gt;', $text);
+			$excerpt_length = apply_filters('excerpt_length', 55);
+			$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+			$text = wp_trim_words( $text, $excerpt_length, $excerpt_more );
+		}
+		return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
+	}
+	
+	add_filter('get_the_excerpt','do_shortcode');
+?>
--- wp-content/themes/simplecorp3/single.php
+++ wp-content/themes/simplecorp3/single.php
@@ -64,29 +64,7 @@
 						 ?>
 						</div>
 
-						<div class="entry-meta">
-							<time class="post-date" datetime="<?php echo the_time('Y-m-d'); ?>">
-								<span class="post-month"><?php the_time('F'); ?></span>
-								<strong class="post-day"><?php the_time('d'); ?></strong>
-								<span class="post-year "><?php the_time('Y'); ?></span>
-							</time>
-							
-							<ul>
-								<li><span class="title"><?php _e("In", "site5framework"); ?>:</span> <?php the_category(', '); ?></li>
-								<li><span class="title"><?php _e("Posted By", "site5framework"); ?>:</span> <?php the_author_posts_link(); ?></li>
-								<?php $post_tags = wp_get_post_tags($post->ID);
-									if(!empty($post_tags)) { ?>
-								<li>
-									<span class="title"><?php _e("Tags", "site5framework"); ?>:</span>
-									<span class="tags">
-										<?php the_tags('', ', ', ''); ?>
-									</span>
-								</li>
-								<?php } ?>
-								<li><span class="title"><?php _e("Comments", "site5framework"); ?>:</span> <?php comments_popup_link(__('0', 'site5framework'), __('1', 'site5framework'), __('%', 'site5framework')); ?></li>
-							</ul>
-						</div>
-
+						
 						<div class="entry-body"> 
 							
 
